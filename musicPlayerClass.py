fdb83f7bc48a08282af392bc795faab15396c216
Gerardo Sandoval <48520321+1sandovalgerardo@users.noreply.github.com>
2019-10-26 09:33:08 -0500

    file for music player app
    this will be the master file.

--- musicPlayerClass.py
+++ musicPlayerClass.py
@@ -6,11 +6,27 @@ from tkinter import ttk
 from tkinter.filedialog import askdirectory
 
 
-root = tk.Tk()
 
-songTitles = []
-songList = []
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
 
-songPlaying = tk.StringVar()
-songPlayingLabel = tk.Label(root)
+    def create_widgets(self):
+        self.hi_there = tk.Button(self)
+        self.hi_there["text"] = "Hello World\n(click me)"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
+
+        self.quit = tk.Button(self, text="QUIT", fg="red",
+                              command=self.master.destroy)
+        self.quit.pack(side="bottom")
+
+    def say_hi(self):
+        print("hi there, everyone!")
+
 root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
