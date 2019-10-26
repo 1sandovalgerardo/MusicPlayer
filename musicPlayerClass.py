eea1a94b177cb8ada4476b45e0d7980996f30049
Gerardo Sandoval <48520321+1sandovalgerardo@users.noreply.github.com>
2019-10-26 11:10:30 -0500

    created working menu bar

--- musicPlayerClass.py
+++ musicPlayerClass.py
@@ -6,26 +6,37 @@ from tkinter import ttk
 from tkinter.filedialog import askdirectory
 
 
-
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
         self.pack()
-        self.create_widgets()
+        self.MenuBar()
+        self.QuitButton()
 
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+    def MenuBar(self):
+        self.menuBar = tk.Menu(self, bg='ivory2')
+        self.fileMenu = tk.Menu(self.menuBar,tearoff=0, relief='sunken')
+        self.fileMenu.add_command(label='Menu Option 1', command=None)
+        self.fileMenu.add_command(label='Menu Option 2', command=None)
+        #self.fileMenu.add_seperator()
+        self.fileMenu.add_command(label='Close', command=self.master.destroy)
+        self.helpMenu = tk.Menu(self.menuBar)
+        self.menuBar.add_cascade(label='File', menu=self.fileMenu)
+        self.menuBar.add_cascade(label='Help', menu=self.helpMenu)
+        self.master.config(menu=self.menuBar)
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
 
-    def say_hi(self):
-        print("hi there, everyone!")
+
+
+    def QuitButton(self):
+        self.quitButton = tk.Button(self, text='Close',
+                                    command=self.master.destroy)
+        self.quitButton.pack()
+
+
+
+
 
 root = tk.Tk()
 app = Application(master=root)
